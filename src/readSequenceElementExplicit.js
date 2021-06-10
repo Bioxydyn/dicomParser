2929132132edbb90659e33800e546c441572c3c2
matthew <matthew.heaton@bioxydyn.com>
2021-06-10 17:28:57 +0100

    Add `tagStartOffset` parameter to element
    Package name
    
    
    Version
    
    
    Version
    
    
    Version
    
    
    Fix publish instructions
    
    
    Version
    
    
    fix: Exploring semantic-release, forcing build
    
    
    Add 'endTagOffset' parameter to element
    
    
    Remove 'endTagOffset'
    
    
    Fix, add 'tagEndOffset'
    
    
    Fix accidental deletion
    
    
    Fix warnings/spelling errors

--- src/readSequenceElementExplicit.js
+++ src/readSequenceElementExplicit.js
@@ -60,6 +60,8 @@ function readSQElementUndefinedLengthExplicit (byteStream, element, warnings) {
 
     const item = readSequenceItemExplicit(byteStream, warnings);
 
+    item.tagEndOffset = byteStream.position;
+
     element.items.push(item);
   }
 
@@ -72,6 +74,8 @@ function readSQElementKnownLengthExplicit (byteStream, element, warnings) {
 
   while (byteStream.position < maxPosition) {
     const item = readSequenceItemExplicit(byteStream, warnings);
+
+    item.tagEndOffset = byteStream.position;
 
     element.items.push(item);
   }
